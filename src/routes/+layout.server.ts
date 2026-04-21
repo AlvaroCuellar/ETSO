cdd5014a146b1ab1c95bd97a10d2970d2349f550
David Merino Recalde <davidmrvg@gmail.com>
2026-04-21 21:52:48 +0200

    intento pass

---
+++ src/routes/+layout.server.ts
@@ -0,0 +1,27 @@
+import { redirect } from '@sveltejs/kit';
+
+import {
+	hasValidAccessCookie,
+	isAccessGateEnabled,
+	normalizeAccessRedirectTarget
+} from '$lib/server/access-gate';
+
+import type { LayoutServerLoad } from './$types';
+
+export const load: LayoutServerLoad = async ({ cookies, url }) => {
+	if (!isAccessGateEnabled()) {
+		return {};
+	}
+
+	const { pathname, search } = url;
+	if (pathname === '/acceso') {
+		return {};
+	}
+
+	if (hasValidAccessCookie(cookies)) {
+		return {};
+	}
+
+	const next = normalizeAccessRedirectTarget(`${pathname}${search}`);
+	throw redirect(303, `/acceso?next=${encodeURIComponent(next)}`);
+};
